4aebb20db8528e58a39f740cc695487df12d618c
smallflyfly <452639095@qq.com>
2020-12-28 10:57:35 +0800

    增加prediction

--- prediction.py
+++ prediction.py
@@ -1,13 +1,22 @@
 # -*- coding: utf-8 -*
+import torch
+from PIL import Image
 from flyai.framework import FlyAI
+from torch.backends import cudnn
+
+from datasets.dataset import FingerPrintDataset
+from model.osnet import osnet_x1_0
+from utils.utils import load_pretrained_weights
 
 
 class Prediction(FlyAI):
-    def load_model(self):
+    def load_model(self, num_classes=100):
         '''
         模型初始化，必须在此方法中加载模型
         '''
-        pass
+        model = osnet_x1_0(num_classes=num_classes)
+        load_pretrained_weights(model, './weights/net_49.pth')
+        return model
 
     def predict(self, image_path_1, image_path_2):
         '''
@@ -15,4 +24,26 @@ class Prediction(FlyAI):
         :param input:  评估传入样例 {"image_path_1":"image\/4900.BMP","image_path_2":"image\/12634.BMP"}
         :return: 模型预测成功之后返回给系统样例 {"label":"1"}
         '''
+        dataset = FingerPrintDataset()
+        model = self.load_model(num_classes=dataset.num_classes)
+        model = model.cuda()
+        model.eval()
+        cudnn.benchmark = True
+        im1 = Image.open('./data/input/FingerprintIdentification/'+image_path_1).convert('L')
+        im2 = Image.open('./data/input/FingerprintIdentification/'+image_path_2).convert('L')
+        im1 = dataset.transform(im1)
+        im2 = dataset.transform(im2)
+        im1 = im1.cuda()
+        im2 = im2.cuda()
+        out1 = model(im1)
+        out1 =torch.sigmoid(out1).cpu().detach().numpy()
+        print(out1)
+        out2 = model(im2)
+        out2 = torch.sigmoid(out2).cpu().detach().numpy()
+        print(out2)
         return {"label":"1"}
+
+
+if __name__ == '__main__':
+    prediction = Prediction()
+    prediction.predict('image/4900.BMP', 'image/12634.BMP')
