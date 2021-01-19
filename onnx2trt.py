afb9b96ec602bbd1e22c66d9bb1bfe712b2b1bf3
smallflyfly <452639095@qq.com>
2021-01-19 16:42:10 +0800

    增加onnx2trt转换文件

---
+++ onnx2trt.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+"""
+@author:fangpf
+@time: 2021/01/19
+"""
+
+import tensorrt as trt
+
+
+onnx_path = './fingerprint.onnx'
+engine_path = 'fingerprint.trt'
+
+TRT_LOGGER = trt.Logger
+
+def onnx2engine():
+
+
+
+if __name__ == '__main__':
+    onnx2engine()
